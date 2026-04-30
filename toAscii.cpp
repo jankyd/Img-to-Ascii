9ce362c1eed2abbe1028fe4a75f58d57bb5ee0d6
Jack Dodge <jackdodge01@gmail.com>
2026-04-30 15:30:39 -0500

    initial argument handling

--- toAscii.cpp
+++ toAscii.cpp
@@ -1,6 +1,18 @@
 #include <stdlib.h>
+#include <iostream>
 #include <opencv2/imgproc.hpp>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage: toAscii [Image file] OR toAscii -h" << std::endl;
+    }
+    
+    else {
+        std::string fileName = argv[1];
+        if (fileName == "-h" || fileName == "-H") {
+            std::cout << "" << std::endl; //TODO: help output
+            return 0;
+        }
+    } 
     return 0;
 }
